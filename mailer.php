fd710cad04aaafc2bc33ee0874c5d78808c8bc4e
Shane325 <Shane325@gmail.com>
2017-03-01 14:43:08 -0800

    Initial commit

---
+++ mailer.php
@@ -0,0 +1,40 @@
+<?php
+
+// Get the form fields, removes html tags and whitespaces
+$name = strip_tags(trim($_POST["name"]));
+$name = str_replace(array("\r","\n"), array(" "," "), $name);
+$phone = strip_tags(trim($_POST["phone"]));
+$phone = str_replace(array("\r","\n"), array(" "," "), $phone);
+$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+$message = trim($_POST["message"]);
+
+// Check the data
+ if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: http://localhost/minerva/views/contact.php?success=-1#form");
+        exit;
+    }
+
+
+// Set the recipient email address
+$recipient = "shane@buildeasyapp.com;shane325@gmail.com";
+
+// Set the email subject
+$subject = "New contact from $name";
+
+// Build the email
+$email_content = "Name: $name\n";
+$email_content .= "Email address: $email\n";
+$email_content .= "Phone number: $phone\n";
+$email_content .= "Message: \n$message\n";
+
+// Build the email headers
+$email_headers = "From: $name <$email>";
+
+// Send the mail
+mail($recipient, $subject, $email_content, $email_headers);
+
+// Redirect to the index.html page with success code
+header("Location: http://localhost/minerva/views/contact.php?success=1#form");
+
+
+?>
---
+++ views/contact.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+  <link rel="shortcut icon" href="../images/favicon.ico" />
+  <title>Contact &#8211; Minerva Construction</title>
+
+  <link rel='stylesheet' href='../css/bootstrap.min.css' type='text/css' media='all' />
+  <link rel='stylesheet' href='../css/font-awesome.min.css' type='text/css' media='all' />
+  <link rel='stylesheet' href='../css/prettyPhoto.css' type='text/css' media='all' />
+  <link rel='stylesheet' href='../css/owl.carousel.css' type='text/css' media='all' />
+  <link rel='stylesheet' href='../css/style.css' type='text/css' media='all' />
+  <link rel='stylesheet' href='../css/custom.css' type='text/css' media='all' />
+  <link rel='stylesheet' href='../css/preloader.css' type='text/css' media='all' />
+
+  <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900' rel='stylesheet' type='text/css'>
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body class="header01">
+  <div id="loading">
+    <div id="loading-center">
+      <div id="loading-center-absolute">
+        <div class="loader">Loading...</div>
+      </div>
+    </div>
+  </div>
+  <div class="snap-drawers">
+    <div class="snap-drawer snap-drawer-left">
+      <div class="mobile-menu">
+        <ul class="menu">
+          <li>
+            <a href="../index.html">Home</a>
+          </li>
+          <li>
+            <a href="about-us.html">Company</a>
+          </li>
+          <li>
+            <a href="services.html">Services</a>
+          </li>
+          <li>
+            <a href="projects.html">Projects</a>
+            <ul class="sub-menu">
+              <li><a href="project_1.html">Candlestick Park</a></li>
+              <li><a href="project_2.html">San Francisco International Airport</a></li>
+              <li><a href="project_3.html">Noe Valley Courts Park</a></li>
+              <li><a href="project_4.html">Highway 101 Drainage</a></li>
+              <li><a href="project_5.html">Burlingame Reservoir &amp; Pump Station</a></li>
+              <li><a href="project_6.html">945 Market Street</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="snap-drawer snap-drawer-right"></div>
+  </div>
+  <div id="page" class="site">
+    <div class="top-area site-top hidden-xs hidden-sm">
+      <div class="container">
+        <div class="row middle">
+          <div class="col-md-6">
+            We Build Structures &amp; Relationships to Last
+          </div>
+          <div class="col-md-6 text-right">
+            <div class="top-menu">
+              <ul id="top-menu" class="menu">
+                <li class="menu-item"><a href="contact-2.html">Contact</a></li>
+              </ul>
+            </div>
+            <div class="social-menu">
+              <ul id="social-menu-top" class="menu">
+                <li><a href="http://facebook.com">Facebook</a></li>
+                <li><a href="http://plus.google.com">Google Plus</a></li>
+                <li><a href="http://twitter.com">Twitter</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <header class="site-header">
+      <nav id="site-navigation" class="main-navigation">
+        <div class="main-menu">
+          <div class="container">
+            <div class="row middle">
+              <div class="col-md-2 col-xs-8 site-branding">
+                <a href="./">
+                  <img src="../images/logo.jpg" alt="Logo" />
+                </a>
+              </div>
+              <div class="col-xs-2 hidden-md hidden-lg end">
+                <i id="open-left" class="fa fa-navicon"></i>
+              </div>
+              <div class="col-md-9 hidden-xs hidden-sm nav-left">
+                <div class="primary-menu">
+                  <ul class="menu">
+                    <li>
+                      <a href="../index.html">Home</a>
+                    </li>
+                    <li>
+                      <a href="about-us.html">Company</a>
+                    </li>
+                    <li>
+                      <a href="services.html">Services</a>
+                    </li>
+                    <li class="menu-item-has-children">
+                      <a href="projects.html">Projects</a>
+                      <ul class="sub-menu">
+                        <li><a href="project_1.html">Candlestick Park</a></li>
+                        <li><a href="project_2.html">San Francisco International Airport</a></li>
+                        <li><a href="project_3.html">Noe Valley Courts Park</a></li>
+                        <li><a href="project_4.html">Highway 101 Drainage</a></li>
+                        <li><a href="project_5.html">Burlingame Reservoir &amp; Pump Station</a></li>
+                        <li><a href="project_6.html">945 Market Street</a></li>
+                      </ul>
+                    </li>
+                    <li class="current-menu-item">
+                      <a href="contact.html">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- #site-navigation -->
+    </header>
+    <div class="site-content">
+      <div class="big-title">
+        <div class="container">
+          <h1 class="entry-title">Contact</h1>
+          <div class="breadcrumb">
+            <div class="container">
+              <ul class="tm_bread_crumb">
+                <li class="top"><a href="../index.html">Home</a></li>
+                <li class="sub tail current">Contact</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="mb-2">
+                <div id="map-canvas" data-address="Alexander Hamilton Bridge" data-height="370" data-width="100%" data-zoom="16" data-map_type="roadmap" data-map_style="default"></div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="mb-2">
+                <h1>
+										We always looking for new collaborations, talents and partners
+									</h1>
+              </div>
+              <div class="contact-info" id="form">
+                <p><i class="fa fa-map-marker"></i>14 Tottenham Road, London, England.</p>
+                <p><i class="fa fa-phone"></i> (102) 6666 8888</p>
+                <p><i class="fa fa-envelope"></i> info@zooka.io</p>
+                <p><i class="fa fa-fax"></i> (102) 8888 9999</p>
+                <p><i class="fa fa-clock-o"></i> Mon – Sat: 9:00 – 18:00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="mb-3">
+                <h2>Send us a message</h2>
+              </div>
+              <form class="wpcf7-form" method="post" action="../mailer.php">
+                <div class="row">
+
+                  <?php
+                  if(isset($_GET['success']) && $_GET['success'] == 1){
+                    echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
+                  }
+                  
+                  if(isset($_GET['success']) && $_GET['success'] == -1){
+                    echo "<div class=\"form-messages error\">Oops! Something went wrong. Please try again.</div>";
+                  }
+                  ?>
+
+                </div>
+
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="wpcf7-form-control-wrap your-name">
+                      <input type="text" name="name" value="" class="wpcf7-form-control" placeholder="Your name (required)" />
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="wpcf7-form-control-wrap your-phone">
+                      <input type="tel" name="phone" value="" class="wpcf7-form-control" placeholder="Your phone" />
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="wpcf7-form-control-wrap your-email">
+                      <input type="email" name="email" value="" class="wpcf7-form-control" placeholder="Your e-mail (required)" />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="wpcf7-form-control-wrap your-message">
+                      <textarea name="message" cols="40" rows="10" class="wpcf7-form-control" placeholder="Your message (required)"></textarea>
+                    </div>
+                    <br />
+                    <input type="submit" value="SUBMIT" class="wpcf7-form-control wpcf7-submit" />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6">
+            <div class="widget">
+              <a href="./" class="footer-logo">
+                <img src="../images/logo_black.jpg" alt="Footer Logo" />
+              </a>
+              Integrity for what we build, the purpose of why we build and the people who inspire us to build. It’s what Builder is built upon, and why our homes are as far from ordinary as the people who live in them.
+            </div>
+            <div class="social">
+              <div class="social-menu">
+                <ul class="menu">
+                  <li><a href="http://facebook.com">Facebook</a></li>
+                  <li><a href="http://plus.google.com">Google Plus</a></li>
+                  <li><a href="http://twitter.com">Twitter</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="widget">
+              <h3 class="widget-title"><span>Information</span></h3>
+              <ul class="menu">
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="./about-us.html">Company</a></li>
+                <li><a href="./services.html">Services</a></li>
+                <li><a href="./projects.html">Projects</a></li>
+                <li><a href="./contact.html">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="widget">
+              <h3 class="widget-title"><span>Contact</span></h3>
+              <div class="textwidget">
+                <p><i class="fa fa-map-marker"></i> &nbsp; 1485 Bayshore Blvd. San Francisco, CA 94124</p>
+                <p><i class="fa fa-phone"></i> &nbsp; +1 (415) 825-5137</p>
+                <p><i class="fa fa-envelope"></i> &nbsp; <a href="mailto:info@minervaconstructioninc.com">info@minervaconstructioninc.com</a></p>
+                <p><i class="fa fa-fax"></i> &nbsp; +1 (415) 825-5137</p>
+                <p><i class="fa fa-clock-o"></i> &nbsp; Mon - Sat: 9:00 - 18:00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <div class="copyright">
+      <div class="container">
+        <div class="row middle">
+          <div class="col-md-12 end-md end-lg center">
+            © Copyrights 2017 Minerva Construction Inc. All rights reserved. Website by <a href="http://shanebarry.com">Shane Barry</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="scrollup"><i class="fa fa-angle-up"></i></a>
+
+  <script type='text/javascript' src='//maps.google.com/maps/api/js?sensor=false&#038;language=en&#038;ver=4.5'></script>
+  <script type='text/javascript' src='../js/jquery.min.js'></script>
+  <script type='text/javascript' src='../js/jquery-migrate.min.js'></script>
+  <script type='text/javascript' src='../js/bootstrap.min.js'></script>
+  <script type='text/javascript' src='../js/snap.min.js'></script>
+  <script type='text/javascript' src='../js/owl.carousel.min.js'></script>
+  <script type='text/javascript' src='../js/jquery.prettyPhoto.js'></script>
+  <script type='text/javascript' src='../js/jquery.prettyPhoto.init.min.js'></script>
+  <script type='text/javascript' src='../js/jQuery.headroom.min.js?ver=1.1.12'></script>
+  <script type='text/javascript' src='../js/headroom.min.js?ver=1.1.12'></script>
+  <script type='text/javascript' src='../js/script.js'></script>
+  <script type='text/javascript' src='../js/gmap3.min.js'></script>
+</body>
+
+</html>
